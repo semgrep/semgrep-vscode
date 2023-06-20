41c2af16af819d3f7110be9f12da54d7d7d8537c
adamboros <boro+github@r2c.dev>
2023-06-20 17:30:22 +0200

    Add walkthrough files

---
+++ walkthrough/3-support.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <img src="support.png" width="100%" alt="Semgrep" />
+</p>
+
+This extension is made to help you. We’d love your feedback to improve it further.
+
+Don't hesitate to
+
+- raise a ticket in [our GitHub repo](https://github.com/returntocorp/semgrep-vscode/issues),
+- or reach out on [Slack](https://go.semgrep.dev/slack).
+
+Step into our Slack community to get answers to your questions and share knowledge with others. We're proud to say no question goes unanswered.
+
+<br/><br/>
+So come aboard, share your experience, <br/>
+and let's _shift left_ together. 💪
---
+++ walkthrough/semgrep-extension.demo.py
@@ -0,0 +1,24 @@
+from cryptography.hazmat.primitives import hashes
+
+### Secret detection
+# ruleid: detected-amazon-mws-auth-token
+TOKEN="amzn.mws.00a19111-8311-5311-5391-5e211cbfcd5c"
+
+### Try autofixing me!
+# ruleid:insecure-hash-algorithm-md5
+hashes.SHA1()
+
+### Ignore a finding
+# ruleid:insecure-hash-algorithm-md5
+hashes.SHA1() # nosem (this line is fine)
+
+
+### ═══════════════════ ✨ Hope you enjoy using Semgrep! ✨ ═══════════════════ ###
+
+# To access all commands, just click Semgrep [ooo] in the bottom right. 
+
+# By default, the extension uses the p/default ruleset,
+# which is a combination of recommended security and best practices rules for many languages.
+
+# You can modify which rules are running in Semgrep Cloud Platform.
+# Try it free by signing up at semgrep.dev!
