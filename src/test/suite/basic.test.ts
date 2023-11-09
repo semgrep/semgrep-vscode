9fa002f5e33bf62aa2dfa81d8d6e4cfd357b969e
ajbt200128 <austin@cutedogs.org>
2023-11-09 13:25:36 -0800

    try running semgrep

--- src/test/suite/basic.test.ts
+++ src/test/suite/basic.test.ts
@@ -1,4 +1,5 @@
 import * as assert from "assert";
+import * as cp from "child_process";
 import { after } from "mocha";
 
 // You can import and use all API from the 'vscode' module
@@ -15,6 +16,11 @@ suite("Extension Test Suite", () => {
     await vscode.workspace.openTextDocument({
       language: "python",
       content: "print('Hello, world!')",
+    });
+    cp.exec("semgrep --version", (err, stdout, stderr) => {
+      assert.strictEqual(err, null);
+      assert.strictEqual(stderr, "");
+      assert.strictEqual(stdout, "0.0.0\n");
     });
     await vscode.commands.executeCommand("semgrep.refreshRules");
   });
