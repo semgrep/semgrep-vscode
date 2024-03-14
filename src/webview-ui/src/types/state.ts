7fea1714ad10b3c7ad3b922b04eba94cf89c6fc0
brandonspark <wu.brandonj@gmail.com>
2024-03-13 17:21:30 -0700

    more comments, and describe possible race condition

--- src/webview-ui/src/types/state.ts
+++ src/webview-ui/src/types/state.ts
@@ -1,5 +1,7 @@
 import { ViewResults } from "./results";
 
+// The state of an ongoing search.
+// We fill up the `results` field as we receive match information!
 export type State = {
   searchConcluded: boolean;
   results: ViewResults;
