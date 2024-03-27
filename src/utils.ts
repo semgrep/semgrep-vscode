711dddb59b9d60939efcac7194145d53a12e79b0
brandonspark <wu.brandonj@gmail.com>
2024-03-27 14:05:21 -0700

    feat: autofix on individual matches

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { tmpdir } from "os";
 import { OutputChannel, Uri } from "vscode";
+import * as vscode from "vscode";
 
 import { LSP_LOG_FILE } from "./constants";
 
@@ -26,4 +27,19 @@ export class Logger {
       this.channel.appendLine(message);
     }
   }
+}
+
+export async function applyFixAndSave(
+  edit: vscode.WorkspaceEdit
+): Promise<void> {
+  const uris = edit.entries().map(([uri, _]) => uri);
+  // According to https://github.com/microsoft/vscode/issues/112109,
+  // adding { isRefactoring: true} should make the fix auto-save.
+  // I couldn't get it to work, though, so let's just save the file manually.
+  await vscode.workspace.applyEdit(edit);
+  await Promise.all(
+    uris.map((uri) =>
+      vscode.workspace.openTextDocument(uri).then((doc) => doc.save())
+    )
+  );
 }
